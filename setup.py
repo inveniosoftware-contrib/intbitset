df8f636069d7d824227c5ca1bd85ccb3e432cabf
Yoan Blanc <yoan@dosimple.ch>
2020-03-08 10:06:38 +0100

    allow empty generator
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of intbitset
-# Copyright (C) 2013, 2014, 2015, 2016 CERN.
+# Copyright (C) 2013, 2014, 2015, 2016, 2020 CERN.
 #
 # intbitset is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License as
@@ -72,6 +72,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         # 'Development Status :: 5 - Production/Stable',
     ],
     test_suite='nose.collector',
